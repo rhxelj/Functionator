c05fa6cc0ec8d549f1c2699a137d4d26d08b62fe
Rogelio M Corsino <rcorsino@gmail.com>
2020-10-28 18:34:09 -0300

    move left and rigth

---
+++ script.js
@@ -0,0 +1,8 @@
+// let body = document.querySelector("body");
+// let H = document.createElement("h1");
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+  //   body.appendChild(h);
+});
+// document.addEventListener("DOMContentLoaded",)
